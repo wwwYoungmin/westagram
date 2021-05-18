9e62c0bfa69ba50ec6329443ff07bc03da1aa0ca
wwwyoungmin <bowwow515@gmail.com>
2021-05-19 00:00:58 +0900

    Add: 내비게이션 바 생성, 아이콘 컴포넌트화 - NavBar 생성, 라우터 연결 - IconSet에 컴포넌트로 생성, NavBar에서 이용

--- src/Routes.js
+++ src/Routes.js
@@ -2,13 +2,17 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Main from "./Pages/Main/Main";
 import Profile from "./Pages/Profile/Profile";
+import Message from "./Pages/Message/Message";
+import NavBar from "./Components/NavBar/NavBar";
 
 function Routes() {
   return (
     <BrowserRouter>
+        <Route component={NavBar} />
       <Switch>
         <Route exact path="/" component={Main} />
         <Route exact path="/profile/:id" component={Profile} />
+        <Route exact path="/profile/:message" component={Message} />
       </Switch>
     </BrowserRouter>
   );
